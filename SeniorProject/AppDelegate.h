ed2659246f9612878d0751e6a484da0ccd7fdbee
Ryley Studer <Ryepod@Macintosh.local>
2014-01-24 15:04:44 -0700

    Initial Commit

---
+++ SeniorProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SeniorProject
+//
+//  Created by Ryley Studer on 1/24/14.
+//  Copyright (c) 2014 Ryley Studer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
